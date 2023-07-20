a717b77a11f3d76dad1a214790cdb9542f6b6160
norman-anderson <thenormanhimself@gmail.com>
2023-07-20 14:35:25 -0400

    Add a new jsp to display the login name and password

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/Login.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/Login.java
@@ -2,13 +2,23 @@ package com.in28minutes.springboot.myfirstwebapp;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class Login {
 
-    @RequestMapping("login")
+    @RequestMapping(value = "login", method = RequestMethod.GET)
     public String gotoLoginPage(){
         return "login";
+    }
+
+    @RequestMapping(value = "login", method = RequestMethod.POST)
+    public String gotoWelcomePage(@RequestParam String name, @RequestParam String password, ModelMap modelMap){
+        modelMap.put("name", name);
+        modelMap.put("password", password);
+        return "welcome";
     }
 }
